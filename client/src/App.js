d48ec300008653d83d8d27b32a0bc270cbe9e785
Daniel Rudd <thisisruddy@gmail.com>
2017-05-17 17:10:43 +0100

    Added semantic-ui-react, semantic-ui-css! Wooh!

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,17 @@
-import React from 'react'
-const App = () => <div>Hello from React! :)</div>
-export default App
+import React, {Component} from 'react';
+import 'semantic-ui-css/semantic.min.css';
+import Layout from './Layout';
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render(){
+    return (
+      <Layout/>
+    );
+  }
+
+}
---
+++ client/src/NavTopbar.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react'
+import {Menu, Segment} from 'semantic-ui-react'
+
+export default class NavTopbar extends Component {
+
+  constructor() {
+    super();
+    this.state = {activeItem: 'home'};
+  }
+
+  handleItemClick({name}){
+    this.setState({activeItem: name})
+  };
+
+  render() {
+    const {activeItem} = this.state;
+
+    return (
+      <Segment inverted>
+        <Menu inverted borderless>
+          <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick}/>
+          <Menu.Item name='messages' active={activeItem === 'messages'} onClick={this.handleItemClick}/>
+          <Menu.Item name='friends' active={activeItem === 'friends'} onClick={this.handleItemClick}/>
+        </Menu>
+      </Segment>
+    )
+  }
+}
